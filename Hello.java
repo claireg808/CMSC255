5085595ff10bd712ac126bf66c16ab12fa6922c0
claireg808 <112021719+claireg808@users.noreply.github.com>
2022-08-24 15:58:45 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/********************
+* Hello
+*********************
+* Claire Gillaspie
+* 8/24/22
+* CMSC255-001
+*********************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Claire Gillaspie>");
+		System.out.println("<Hello>");
+		System.out.println("<Name design>");
+		System.out.println("<8/24/22>");
+		System.out.println("<CMSC255-001>");
+		System.out.println();
+}
+
+	}
